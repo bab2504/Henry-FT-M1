912a1f834eb6986015dca7a78a81f5803b8171aa
Boris Andrés <bandresberrocal@gmail.com>
2022-04-06 23:23:29 -0300

    homework00

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,6 +2,14 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var array = num.split('')
+  var sum=0
+
+  for(var i=0; i<array.length; i++) {
+    sum = sum + Math.pow(2, array.length-1-i) * array[i]
+  }
+  
+  return sum;
 
 }
 
